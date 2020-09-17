851ca2e95f0dfe4529a58754639c90e0bbda7926
OrangeX4 <318483724@qq.com>
2020-09-17 10:27:57 +0800

    Add something

---
+++ src/components/Item.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img src="../assets/folder-close.png" width="50" height="50">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Item',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div * {
+    vertical-align: middle;
+  }
+
+  div {
+    padding: 0;
+    margin: 0;
+    width: 100%
+  }
+</style>
