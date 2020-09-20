642ab505aa493decdcec3f890faaaa041b5e61a0
OrangeX4 <318483724@qq.com>
2020-09-20 20:56:29 +0800

    Add . folder

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <breadcrumb text="/home"></breadcrumb>
-  <Item name=".." description="Return to parent" icon="folder"></Item>
+  <Item name=".." description="Return to parent folder" icon="folder"></Item>
+  <Item name="." description="Refresh current folder" icon="folder"></Item>
   <Item name="Folder 2" description="3 Items" icon="folder"></Item>
   <Item name="File 1" description="233 KiB" icon="file"></Item>
 </template>
