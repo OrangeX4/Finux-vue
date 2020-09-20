ada56ed4be41688237b495ab6a0e015a954177a4
OrangeX4 <318483724@qq.com>
2020-09-20 20:03:07 +0800

    Del clipboard

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import { createApp, use } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import VueClipboard from 'vue-clipboard2'
+// import VueClipboard from 'vue-clipboard2'
 import store from './store'
 
 
 
 createApp(App).use(store).mount('#app')
-
-
-use(VueClipboard)
